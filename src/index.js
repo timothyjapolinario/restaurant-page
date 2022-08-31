9da85cb034fb9d3e4511dc0a0aada3f9520bc0f7
JayJay <timothyjapolinario@gmail.com>
2022-08-31 18:20:18 +0800

    Add header design

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import {addComponent as renderHeader} from './modules/header.js';
 const content = document.querySelector('#content')
 
-console.log(content)
 renderHeader(content)
--- src/modules/header.js
+++ src/modules/header.js
@@ -5,8 +5,6 @@ import Logo from '../images/logo.png'
 const addComponent = (contentBody)=>{
     const header = document.createElement('div')
     header.classList.add(('header'))
-
-    console.log(contentBody)
     addPageLogo(header)
     addMenuOptions(header)
     contentBody.appendChild(header)
@@ -15,13 +13,14 @@ const addComponent = (contentBody)=>{
 
 const addPageLogo = (header) =>{
     const logoAndName = document.createElement('div')
+    logoAndName.classList.add('logo-name')
     const name = document.createElement('span')
     name.innerText = "Third-Round Knockout"
-    name.classList.add("lolll-name")
-    // const myLogo = new Image()
-    // myLogo.src = Logo
-    // myLogo.id = "logo"
-    // logoAndName.appendChild(myLogo)
+    name.classList.add("page-name")
+    const myLogo = new Image()
+    myLogo.src = Logo
+    myLogo.id = "logo"
+    logoAndName.appendChild(myLogo)
     logoAndName.appendChild(name)
 
 
@@ -33,17 +32,32 @@ const addMenuOptions = (header)=>{
     const options = document.createElement('div')
     options.classList.add('option-list')
 
+    options.chi
     const menu = document.createElement('div')
     menu.classList.add('header-option')
     menu.innerText = "Menu"
+    menu.addEventListener('click',()=>{
+        clearSelectedClass(options)
+        addSelectedClass(menu)
+    })
 
     const about = document.createElement('div')
     about.classList.add('header-option')
     about.innerText = "About"
+    about.addEventListener('click',()=>{
+        clearSelectedClass(options)
+        addSelectedClass(about)
+    })
 
     const contact = document.createElement('div')
     contact.classList.add('header-option')
-    about.innerText = "Contact"
+    contact.innerText = "Contact"
+    contact.addEventListener('click',()=>{
+        clearSelectedClass(options)
+        addSelectedClass(contact)
+    })
+    
+
 
 
     options.appendChild(menu)
@@ -52,6 +66,14 @@ const addMenuOptions = (header)=>{
     header.appendChild(options)
 }
 
+const addSelectedClass = (option) =>{
+    option.classList.add('selected')
+}
+const clearSelectedClass = (options)=>{
+    options.childNodes.forEach(element => {
+        element.classList.remove('selected')
+    });
+}
 export{
     addComponent
 }
